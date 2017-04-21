84edf0524dae959327b2600a2cab0840ce351c5f
da99 <i-hate-spam-1234567@mailinator.com>
2017-04-21 14:16:33 -0500

    Added: watch

---
+++ bin/public/watch/_.sh
@@ -0,0 +1,29 @@
+
+# === {{CMD}}
+watch () {
+  local +x PATH="$PATH:$THIS_DIR/../sh_color/bin"
+  PATH="$PATH:$THIS_DIR/../mksh_setup/bin"
+  PATH="$PATH:$THIS_DIR/bin"
+  PATH="$PATH:$THIS_DIR/progs/latest-crystal/bin"
+  PATH="$PATH:$THIS_DIR/progs/latest-shards/bin"
+
+  cd "$THIS_DIR"
+
+  if [[ ! -z "$@" ]] ; then
+    echo "=== Compiling... $(date "+%H:%M:%S") ..." >&2
+
+    local +x FILE="tmp/scratch.cr"
+    if [[ ! -f "$FILE" ]]; then
+      sh_color "=== File does not exist: {{$FILE}}"
+    fi
+
+    crystal build tmp/scratch.cr
+    mv scratch tmp/
+    tmp/scratch
+    echo ""
+  else
+    mksh_setup watch "-r bin -r tmp " "$THIS_DIR/bin/my_crystal_lang watch run"
+  fi
+
+} # === end function
+
