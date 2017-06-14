2c034632b03a336e9c9535c17a65070992b93a28
da99 <i-hate-spam-1234567@mailinator.com>
2017-06-13 22:25:22 -0500

    Fixed: typo.

--- bin/public/crystal/_.sh
+++ bin/public/crystal/_.sh
@@ -2,6 +2,6 @@
 # === {{CMD}}  -args -to -crystal binary
 crystal () {
   unset -f crystal
-  PATH="$PATH:$THIS_DIR/progs/latest/bin"
+  PATH="$PATH:$THIS_DIR/progs/latest-crystal/bin"
   crystal "$@"
 } # === end function
